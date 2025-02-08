09007b43f39c1d5d3d785e7f8d10b719f519d9ab
morganhel <138800018+morganhel@users.noreply.github.com>
2025-02-08 22:19:46 +0100

    ip vps

--- my-strapi-app/config/database.ts
+++ my-strapi-app/config/database.ts
@@ -1,13 +1,25 @@
-export default ({ env }) => {
-  return {
+// export default ({ env }) => {
+//   return {
+//     connection: {
+//       client: "postgres",  // Force PostgreSQL
+//       connection: {
+//         connectionString: env("DATABASE_URL"),
+//         ssl: env.bool("DATABASE_SSL", true) ? { rejectUnauthorized: false } : false,
+//       },
+//       pool: { min: 2, max: 10 },
+//       acquireConnectionTimeout: 60000,
+//     },
+//   };
+// };
+
+import path from 'path';
+
+export default ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client: "postgres",  // Force PostgreSQL
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        ssl: env.bool("DATABASE_SSL", true) ? { rejectUnauthorized: false } : false,
-      },
-      pool: { min: 2, max: 10 },
-      acquireConnectionTimeout: 60000,
+      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
